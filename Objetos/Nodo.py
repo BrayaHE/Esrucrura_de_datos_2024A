94a1c61a5ca54284b735111cb2ae799a30d14b5d
BrayanHe <2202051@universidad-une.com>
2024-02-12 12:09:11 -0600

    Listas completas
    Lista ligadas,Doblemente ligadas,Circulares

--- Objetos/Nodo.py
+++ Objetos/Nodo.py
@@ -1,18 +1,34 @@
-''' Modulo de contenedor de clase Nodo para listas ligadas '''
+# ''' Modulo de contenedor de clase Nodo para listas ligadas '''
 
-class Nodo:
-    '''Creacion de Nodo'''
+# class Nodo:
+#     '''Creacion de Nodo'''
 
-    def __init__(self, val: int = None):
-        '''Inicializacion de clase'''
-        self.val = val
-        self.next = None
+#     def __init__(self, val: int = None):
+#         '''Inicializacion de clase'''
+#         self.val = val
+#         self.next = None
 
-    def __str__(self) -> str:
-        if self.val:
-            return f'Valor del Nodo = {self.val}'
-        return f'El nodo no tiene valor'
+#     def __str__(self) -> str:
+#         if self.val:
+#             return f'Valor del Nodo = {self.val}'
+#         return f'El nodo no tiene valor'
+
+# '''Lista doblemente ligadas'''
+
+
+# '''modulo contenedor de clase '''
+
+# class nodo:
+#     def _init_(self,  val:int = None ):
+#         '''iniciliciador de clase'''
         
+#         self.val = val
+#         self.nextn = None 
+
+#         def _str_(self)-> str :
+#             if self.val:
+#                 return f'valor de nodo = {self.val}:'
+#                 return f' el nodo no tiene valor'
 
 
 
--- Objetos/conversion.py
+++ Objetos/conversion.py
@@ -15,7 +15,7 @@
 #         print(head)
 #         head = head.next
         
-'''Codigo en clase  while'''
+# '''Codigo en clase  while'''
 # from Nodo import Nodo
 
 # e = [1, 2, 3, 4, 5]
@@ -34,26 +34,52 @@
 #         print(temp)
 #         temp = temp.next
 
+'''Listas doblemente ligadas'''
+import nodo
 
-
-'''Listas ligadas cirulares'''
-from Nodo import Nodo
 e = [1, 2, 3, 4, 5]
-i=0
-if __name__ == '__main__':
-    head = Nodo()
+i = 0
+
+if _name_ == '_main_':
+    head = nodo.nodo()  
     curr = head
-    for i in e:
-        curr.val = i
-        curr.next = Nodo()
-        curr = curr.next 
-        
-        curr.next=head 
-        
+    
+    while i < len(e):
+        curr.val = e[i]
+        if i < len(e) - 1:  
+            curr.nextn = nodo.nodo()  
+            curr.nextn.prevn = curr  # Establecer el nodo actual como el anterior del siguiente nodo
+            curr = curr.nextn
+        i += 1
+
     curr = head
-    for _ in range(len(e)):
-        print(curr.val)
-        curr = curr.next        
+    while curr is not None:
+        print("Nodo actual:", curr.val, end=" ")
+        if hasattr(curr, 'prevn') and curr.prevn:
+            print(", Anterior:", curr.prevn.val, end=" ")
+        if hasattr(curr, 'nextn') and curr.nextn:
+            print(", Siguiente:", curr.nextn.val, end=" ")
+        print()
+        curr = curr.nextn
+
+# '''Listas ligadas cirulares'''
+# from Nodo import Nodo
+# e = [1, 2, 3, 4, 5]
+# i=0
+# if __name__ == '__main__':
+#     head = Nodo()
+#     curr = head
+#     for i in e:
+#         curr.val = i
+#         curr.next = Nodo()
+#         curr = curr.next 
+        
+#         curr.next=head 
+        
+#     curr = head
+#     for _ in range(len(e)):
+#         print(curr.val)
+#         curr = curr.next        
 
 
 
